a72e245df0c7a56e70eb63119848756ac1fc0932
Joshua Mclean <joshmcleana@gmail.com>
2020-02-26 06:43:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,11 +19,12 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.times { |index| if array[index] > x
+  array.length.times { |index|
+    if array[index] > x
       x = array[index]
-end
-}
-x
+    end
+  }
+  x
 end
 
 # def find_min_value(array)
